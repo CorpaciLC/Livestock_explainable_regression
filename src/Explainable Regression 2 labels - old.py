c0c38e27da3c0efeacee4fd562420b481dbca87c
Luiza Corpaci <corpaci.luiza@yahoo.com>
2024-08-05 08:36:48 +0200

    Uploaded latest edits

---
+++ src/Explainable Regression 2 labels - old.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# coding: utf-8
+#%% Imports
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+from sklearn.model_selection import train_test_split
+from ml_models import dt, rf, catb, gradb
+
+from sklearn.inspection import PartialDependenceDisplay
+import lime
+from lime import lime_tabular
+import shap
+from IPython.display import display
+
+from selenium import webdriver
+from PIL import Image
+
+
+#%% Configuration
+
+no_clusters= 2
+question = 'labels'
+
+current_path = os.path.dirname(os.getcwd())
+DATA_PATH = os.path.join(current_path, 'data\\')
+IMAGES_PATH = os.path.join(current_path, 'images\\')
+
+filename = DATA_PATH + f'Livestock_combined_kmeans_{no_clusters}labels.csv'
+
+df = pd.read_csv(filename)
+df.head()
+
+#%% Train/Test Split
+
+df_aux = df.copy()
+X = df_aux.drop(question, axis=1)
+y = df_aux[question].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+#%%
+count_matrix = pd.crosstab(df['labels'], df['Q6. TECH usage'])
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(count_matrix, annot=True, cmap="YlGnBu", fmt='g')  # 'fmt' is used to format numbers as integers
+plt.title('Heatmap of Count Data for Q6. TECH Usage and Labels')
+plt.xlabel('Q6. TECH Usage')
+plt.ylabel('Labels')
+plt.show()
+
+
+
+
+#%% ML Models
+
+clf_dt, y_pred_dt, precision_dt, recall_dt, accuracy_dt, cv_results_dt = dt(X_train, y_train, X_test, y_test, X, y)
+
+clf_rf, y_pred_rf, precision_rf, recall_rf, accuracy_rf, cv_results_rf = rf(X_train, y_train, X_test, y_test, X, y)
+
+clf_gradb, y_pred_gradb, precision_gradb, recall_gradb, accuracy_gradb, cv_results_gradb = gradb(X_train, y_train, X_test, y_test, X, y)
+
+clf_catb, y_pred_catb, precision_catb, recall_catb, accuracy_catb, cv_results_catb = catb(X_train, y_train, X_test, y_test, X, y)
+
+
+
+#%%
+ml_models ={
+    'dt': dt(X_train, y_train, X_test, y_test, X, y),
+    'rf': rf(X_train, y_train, X_test, y_test, X, y),
+    'gradb': gradb(X_train, y_train, X_test, y_test, X, y),
+    'catb': catb(X_train, y_train, X_test, y_test, X, y)
+}
+
+for clf_model, clf_details in ml_models.items():
+    clf = clf_details[0]
+    y_pred = clf_details[1]
+    precision = clf_details[2]
+    recall = clf_details[3]
+    accuracy = clf_details[4]
+    cv_results = clf_details[5]
+
+    IMAGES_PATH_clf = IMAGES_PATH + clf_model + '\\' + f'{no_clusters}labels\\'
+    os.makedirs(IMAGES_PATH_clf, exist_ok=True)
+
+    if not os.path.exists(IMAGES_PATH_clf + 'experiment_details.txt'):
+        text = f"Model: {clf}, \nNumber Labels: {no_clusters}, \nAccuracy= {accuracy}, \nPrecision={precision}, \nRecall={recall}, \nCross-validation={cv_results}"
+        with open(IMAGES_PATH_clf + 'experiment_details.txt', "w") as file:
+            file.write(text)
+        print(text)
+
+     
+    features = list(range(X_train.shape[1])) 
+    ########################## Partial Dependence Plots  ##########################
+    desired_value = 2
+    fig, ax = plt.subplots(nrows=8, ncols=3, figsize=(20, 20), constrained_layout=True)
+
+    for i, feature in enumerate(features):
+        row, col = divmod(i, 3)
+        PartialDependenceDisplay.from_estimator(
+            clf, 
+            X_train, 
+            [feature], 
+            kind='both', 
+            grid_resolution=50, 
+            ax=ax[row, col],
+            feature_names=X_train.columns,
+            target=desired_value
+        )
+
+
+    for i in range(len(features), 24):
+        row, col = divmod(i, 3)
+        fig.delaxes(ax[row, col])
+
+    plt.suptitle('Partial Dependence and ICE Plots for Features', fontsize=16)
+    plt.savefig(IMAGES_PATH_clf + f'PDP_ICE_{no_clusters}labels_{clf_model}.png')
+    plt.show()
+
+
+    ########################## LIME  ##########################
+
+    explainer = lime.lime_tabular.LimeTabularExplainer(
+        training_data=X_train.values,
+        feature_names=X_train.columns,
+        class_names=list(set(y)), 
+        mode='regression')
+
+    for instance_index in range(10):
+        instance = X_test.iloc[instance_index]
+
+        exp = explainer.explain_instance(
+            data_row=instance,
+            predict_fn=clf.predict, 
+            num_features=10 
+        )
+        exp = explainer.explain_instance(instance.values, clf.predict, num_features=len(features))
+        
+        fig = plt.figure(figsize=(16, 6))
+        fig = exp.as_pyplot_figure(fig)
+
+        image_path = IMAGES_PATH_clf + f'lime_{no_clusters}labels_instance{instance_index}_{clf_model}.png'
+        fig.savefig(image_path, bbox_inches='tight')
+        plt.close(fig)
+
+
+    ########################## Shapely  ##########################
+
+    explainer = shap.TreeExplainer(clf)
+    expl_shap_values = explainer(X_test)
+
+    shap.initjs()
+
+    for instance_index in range(10):
+        plt.figure(figsize=(12, 4)) 
+        shap_waterfall_plot = shap.plots.waterfall(expl_shap_values[instance_index], max_display=20, show=False)
+        
+        fig = plt.gcf()
+        instance_label = f"{y_test[instance_index]}"
+        title = f"Label: {instance_label}, Clusters: {no_clusters}, Model: {clf_model}"
+        plt.title(title, fontsize=12)
+        
+        image_path = IMAGES_PATH_clf + f'shap_{no_clusters}clusters_instance{instance_index}_{clf_model}.png'
+        fig.savefig(image_path, bbox_inches='tight', dpi=300)
+        plt.show()
+
+
+
+
+# %%
