166fce21f4fa51bff00b71ae5f15b37d340fabc8
Luiza Corpaci <corpaci.luiza@yahoo.com>
2024-12-06 12:39:46 +0100

    edditted config

--- src/config.py
+++ src/config.py
@@ -5,4 +5,7 @@ current_path = os.path.dirname(os.getcwd())
 DATA_PATH = os.path.join(current_path, 'data\\')
 IMAGES_PATH = os.path.join(current_path, 'images\\')
 
+if not os.path.exists(IMAGES_PATH):
+    os.makedirs(IMAGES_PATH)
+
 filename = DATA_PATH + f'Livestock_combined_kmeans_{no_clusters}labels.csv'
